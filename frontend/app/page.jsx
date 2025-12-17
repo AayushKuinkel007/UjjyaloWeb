07746d52d5f6ead11b93e3a5f815e3e802bfae29
Aayush Kuinkel <codewithaayush007@gmail.com>
2025-12-17 10:36:50 +0545

    frontend add

---
+++ frontend/app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <h1 className='text-5xl text-[#f7a11f] text-center font-semibold mt-3'>Hello World</h1>
+    </>
+  )
+}
+
+export default page
