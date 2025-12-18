b6b03662edfee5846d92b75f4ff7955d2e81f5fa
Aayush Kuinkel <codewithaayush007@gmail.com>
2025-12-18 22:11:50 +0545

    check icon add

--- frontend/app/Components/Footer.jsx
+++ frontend/app/Components/Footer.jsx
@@ -7,11 +7,7 @@ const Footer = () => {
     <>
       <footer>
         <div className="flex justify-center">
-          <button
-            className="p-3 bg-[#f48c25] rounded-xl text-black"
-          >
-            FAQ's
-          </button>
+          
         </div>
       </footer>
     </>
